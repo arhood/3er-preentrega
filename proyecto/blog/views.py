16130ea88963677da71c9781b58bb5c848174550
Hood Adrian <fact.serv.casa@gmail.com>
2023-11-05 23:27:40 -0300

    Creado de html con estilo, modelos, principio de views.

--- proyecto/blog/views.py
+++ proyecto/blog/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
+def usuarios (request):
+    contexto = {''}
+    http_response = render(
+        request = request,
+        tamplate_name = 'proyecto/usuarios.html',
+        context = contexto,
+    )
+    return http_response
+
+def temas (request):
+    contexto = {''}
+    http_response = render(
+        request = request,
+        tamplate_name = 'proyecto/temas.html',
+        context = contexto,
+    )
+    return http_response
+
+def posts (request):
+    contexto = {''}
+    http_response = render(
+        request = request,
+        tamplate_name = 'proyecto/posts.html',
+        context = contexto,
+    )
+    return http_response
+
+def inicio (request):
+    contexto = {''}
+    http_response = render(
+        request = request,
+        tamplate_name = 'proyecto/base.html',
+        context = contexto,
+        )
+    return http_response
